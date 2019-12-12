e999550750107a32d33afff641bd9fe8f48676c0
Cem Ergin <birazgecemuzigi@gmail.com>
2019-12-12 14:39:33 -0500

    Hello World v3.0

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,60 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {cities: []}
+  // Initialize state
+  state = { passwords: [] }
 
-  async componentDidMount() {
-    const response = await fetch('/cities')
-    const cities   = await response.json()
+  // Fetch passwords after first mount
+  componentDidMount() {
+    this.getPasswords();
+  }
 
-    this.setState({cities: cities})
+  getPasswords = () => {
+    // Get the passwords and store them in state
+    fetch('/api/passwords')
+      .then(res => res.json())
+      .then(passwords => this.setState({ passwords }));
   }
 
   render() {
+    const { passwords } = this.state;
+
     return (
-      <div>
-        <h2>{process.env.REACT_APP_MY_VARIABLE}</h2>
-        <ul>
-          {this.state.cities.map( city => {
-            return <li key={city.name}> <b>{city.name}</b>: {city.population}</li>
-          })}
-        </ul>
+      <div className="App">
+        {/* Render the passwords if we have them */}
+        {passwords.length ? (
+          <div>
+            <h1>5 Passwords.</h1>
+            <ul className="passwords">
+              {/*
+                Generally it's bad to use "index" as a key.
+                It's ok for this example because there will always
+                be the same number of passwords, and they never
+                change positions in the array.
+              */}
+              {passwords.map((password, index) =>
+                <li key={index}>
+                  {password}
+                </li>
+              )}
+            </ul>
+            <button
+              className="more"
+              onClick={this.getPasswords}>
+              Get More
+            </button>
+          </div>
+        ) : (
+          // Render a helpful message otherwise
+          <div>
+            <h1>No passwords :(</h1>
+            <button
+              className="more"
+              onClick={this.getPasswords}>
+              Try Again?
+            </button>
+          </div>
+        )}
       </div>
     );
   }
