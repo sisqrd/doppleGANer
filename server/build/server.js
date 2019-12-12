4f2605879f1faeac1003f678027183119f981d6a
Cem Ergin <birazgecemuzigi@gmail.com>
2019-12-12 01:34:44 -0500

    Hello World v2.0

--- server/build/server.js
+++ server/build/server.js
@@ -8,6 +8,10 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
@@ -17,12 +21,18 @@ app.use(_bodyParser2.default.urlencoded({ extended: false }));
 
 var router = _express2.default.Router();
 
+var staticFiles = _express2.default.static(_path2.default.join(__dirname, '../../client/build'));
+
+app.use(staticFiles);
+
 router.get('/cities', function (req, res) {
   var cities = [{ name: 'New York City', population: 8175133 }, { name: 'Los Angeles', population: 3792621 }, { name: 'Chicago', population: 2695598 }];
   res.json(cities);
 });
 
 app.use(router);
+
+app.use('/*', staticFiles);
 
 app.set('port', process.env.PORT || 3001);
 
